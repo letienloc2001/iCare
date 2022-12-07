faeaac39004e9a67d8490c2a44d57bfcea016f84
Nguyễn Khương Duy <duynguyen60201@gmail.com>
2022-12-08 02:04:26 +0700

    edit profile

--- models/user.php
+++ models/user.php
@@ -1,22 +1,24 @@
 <?php
 
-class UserModel {
-    public function InitConnect(){
+class UserModel
+{
+    public function InitConnect()
+    {
         $con = mysqli_connect('localhost', 'root', 'root', 'iCare');
 
-        if (mysqli_connect_errno()){
-            die('Connection failed: '. mysqli_connect_error());
-        }
-        else return $con;
+        if (mysqli_connect_errno()) {
+            die('Connection failed: ' . mysqli_connect_error());
+        } else return $con;
     }
 
-    public function getAllUser(){
+    public function getAllUser()
+    {
         $con = $this->InitConnect();
 
         $res = $con->query('SELECT * FROM user_table');
         $users = array();
-        if ($res->num_rows > 0){
-            while ($user = mysqli_fetch_assoc($res)){
+        if ($res->num_rows > 0) {
+            while ($user = mysqli_fetch_assoc($res)) {
                 $users[] = $user;
             }
         }
@@ -25,34 +27,34 @@ class UserModel {
         return $users;
     }
 
-    public function getOneUser($email){
+    public function getOneUser($email)
+    {
         $con = $this->InitConnect();
 
-        $res = $con->query("SELECT * FROM user WHERE Email='".$email."'");
+        $res = $con->query("SELECT * FROM user WHERE Email='" . $email . "'");
         $user = mysqli_fetch_assoc($res);
 
         $con->close();
         return $user;
     }
 
-    public function getOneUserName($id){
+    public function getOneUserName($id)
+    {
         $con = $this->InitConnect();
         if ($_SESSION['user_type'] == 'a') {
-            $res = $con->query("SELECT admin_name FROM admin WHERE admin_id=".$id);
+            $res = $con->query("SELECT admin_name FROM admin WHERE admin_id=" . $id);
             return $res->fetch_assoc()['admin_name'];
-        }
-        else if ($_SESSION['user_type'] == 'p') {
-            $res = $con->query("SELECT patient_name FROM patient WHERE patient_id=".$id);
+        } else if ($_SESSION['user_type'] == 'p') {
+            $res = $con->query("SELECT patient_name FROM patient WHERE patient_id=" . $id);
             return $res->fetch_assoc()['patient_name'];
-        }
-        else {
-            $res = $con->query("SELECT doc_name FROM doctor WHERE doc_id=".$id);
+        } else {
+            $res = $con->query("SELECT doc_name FROM doctor WHERE doc_id=" . $id);
             return $res->fetch_assoc()['doc_name'];
-
         }
     }
 
-    public function changeUserData($email, $password, $phone, $avatar){
+    public function changeUserData($email, $password, $phone, $avatar)
+    {
         // check valid step first
         $con = $this->InitConnect();
 
@@ -66,7 +68,8 @@ class UserModel {
         return true;
     }
 
-    public function deleteOneUser($email){
+    public function deleteOneUser($email)
+    {
         $con = $this->InitConnect();
         $res = $con->query("DELETE FROM user_table WHERE Email='$email'");
         $con->close();
@@ -77,15 +80,13 @@ class UserModel {
         $con = $this->InitConnect();
 
         $res = $con->query("SELECT Password FROM user WHERE Email = '$email'");
-        if (mysqli_num_rows($res) == 0){
+        if (mysqli_num_rows($res) == 0) {
             return 0;
-        }
-        else {
-            if ($password == $res->fetch_assoc()['Password']){
+        } else {
+            if ($password == $res->fetch_assoc()['Password']) {
                 $con->close();
                 return 1;
-            }
-            else {
+            } else {
                 $con->close();
                 return 0;
             }
@@ -103,15 +104,15 @@ class UserModel {
 
         $res = $con->query("SELECT * FROM user_table WHERE Email = '$email'");
         if (mysqli_num_rows($res) == 0) {
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 $con->close();
                 return 4;       // 4 is email
             }
-            if (strlen($phone) > 13 || strlen($phone) < 9){
+            if (strlen($phone) > 13 || strlen($phone) < 9) {
                 $con->close();
                 return 5;       // 5 is phone
             }
-            if (!preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#", $password)){
+            if (!preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#", $password)) {
                 $con->close();
                 return 3;       // 3 is password
             }
@@ -120,18 +121,17 @@ class UserModel {
             if ($con->query($sql) == TRUE) {
                 $con->close();
                 return 1;       // 1 is ok
-            }
-            else {
+            } else {
                 $con->close();
                 return -1;      // query
             }
-        }
-        else {
+        } else {
             return 0;           // 0 is duplicate
         }
     }
 
-    public function getPictureOneUser($id){
+    public function getPictureOneUser($id)
+    {
         $con = $this->InitConnect();
         if ($_SESSION['user_type'] == 'a') {
             return "";
@@ -143,5 +143,3 @@ class UserModel {
         return $res->fetch_assoc()['image_url'];
     }
 }
-
-?>
