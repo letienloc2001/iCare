55a77a3150cea2aad1978bf680c6247c1a62d2ec
tranhungcuong-hub <80353022+tranhungcuong-hub@users.noreply.github.com>
2022-12-08 13:06:12 +0700

    first commitaa

--- views/detail-components/detail-info-card.php
+++ views/detail-components/detail-info-card.php
@@ -5,6 +5,9 @@ include "./services/connection.php";
     $username = "";
     if(isset($_SESSION['user_type']))
         $user_type = $_SESSION['user_type'];
+    if(isset($_SESSION['id'])){
+        $pat_id = $_SESSION['id'];
+    }
     $name = "";
     $image_url = "";
     $address = "";
@@ -38,9 +41,10 @@ include "./services/connection.php";
     <div>
         <div class="user-name"><?php echo $name ?></div>
         <?php if($user_type == 'p') { ?>
-        <button class="edit-button"><a href="./services/booking-process.php?doc_id=<?php echo $id ?>&pat_id=<?php echo $_SESSION['id'] ?>" class="edit-profile"> Request <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+            <button class="edit-button"><a href="./services/booking-process.php?doc_id=<?php echo $id ?>&pat_id=<?php echo $pat_id ?>" class="edit-profile"> Request <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
                     <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
-                </svg></a></button>
+                </svg></a>
+            </button>
         <?php } ?>
         <div class="user-info">Specializations: <span class="info"></span></div>
         <?php
