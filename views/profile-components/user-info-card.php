ffcea886d311eb5966676d8c171c4c9705d90ded
tranhungcuong-hub <80353022+tranhungcuong-hub@users.noreply.github.com>
2022-12-08 12:53:23 +0700

    asdasd

--- views/profile-components/user-info-card.php
+++ views/profile-components/user-info-card.php
@@ -41,11 +41,11 @@ include "./services/connection.php";
 <div class="user-card">
     <?php if ($user_type === 'd') { ?>
         <div class="user-image-card">
-            <img class="user-image" src="<?php echo $image_url ?>" alt="">
+            <img class="user-image" src="<?php echo $pic ?>" alt="">
         </div>
     <?php } else if ($user_type === 'p') { ?>
         <div class="user-image-card">
-            <img class="user-image" src="<?php echo $image_url ?>" alt="">
+            <img class="user-image" src="<?php echo $pic ?>" alt="">
         </div>
     <?php } ?>
     <div>
@@ -53,7 +53,9 @@ include "./services/connection.php";
         <button class="edit-button"><a href="edit-profile.php" class="edit-profile">Edit Profile <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
                     <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
                 </svg></a></button>
-        <div class="user-info">Address: <span class="info"><?php echo $address ?></span></div>
+        <?php if($user_type != 'a') {?>
+            <div class="user-info">Address: <span class="info"><?php echo $address ?></span></div>
+        <?php } ?>
         <?php if ($user_type === 'p') { ?>
             <div class="user-info">Identity number: <span class="info"><?php echo $nic ?></span></div>
         <?php } else if ($user_type === 'd') { ?>
